c296a811b96903f0eb57aad92188e36399b9da7d
thamaragerigr <thamaragerigr@gmail.com>
2020-04-19 20:10:11 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Pokedex from './Pokedex';
+import Pokegame from './Pokegame';
+function App() {
+  return (
+    <div className="App">
+     <Pokegame
+     />
+    </div>
+  );
+}
+
+export default App;
